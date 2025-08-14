db41365921a03f7f0b38e102096ce213a656b66d
Silver <silver@squirl.dev>
2025-08-14 19:33:01 +0100

    add CI

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   description = "A Nix flake for the Zig programming language";
   inputs.nixpkgs.url = "nixpkgs";
-  outputs = {nixpkgs, ...}: {
-    packages = builtins.mapAttrs (system: pkgs: import ./versions {inherit pkgs;}) nixpkgs.legacyPackages;
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    packages =
+      builtins.mapAttrs (
+        system: pkgs:
+          import ./versions {inherit pkgs;}
+          // {default = self.packages.${system}.zig;}
+      )
+      nixpkgs.legacyPackages;
   };
 }
