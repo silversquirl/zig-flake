1b6c0657170a829e8420306a1ad7b5555647cdae
Silver <silver@squirl.dev>
2025-08-18 14:39:37 +0100

    refactor: un-genericify packaging logic

--- flake.nix
+++ flake.nix
@@ -3,18 +3,38 @@
   inputs.nixpkgs.url = "nixpkgs";
   outputs = {nixpkgs, ...}: {
     packages = let
-      lib = nixpkgs.lib;
-      snakeify = version:
-        lib.pipe version [
-          lib.versions.splitVersion # parse version
-          (lib.sublist 0 5) # strip commit hash
-          (lib.concatStringsSep "_") # recombine
-        ];
-    in
-      import ./flake-packages.nix {
-        inherit nixpkgs;
-        name = kind: release: "zig_${snakeify release._version}";
-        defaultNightlyName = "nightly";
+      inherit (nixpkgs) lib;
+
+      stable = import ./releases/stable.nix;
+      nightly = import ./releases/nightly.nix;
+
+      named = release: {
+        name =
+          "zig_"
+          + lib.pipe release._version [
+            lib.versions.splitVersion # parse version
+            (lib.sublist 0 5) # strip commit hash
+            (lib.concatStringsSep "_") # recombine
+          ];
+        value = release;
       };
+
+      releases =
+        builtins.listToAttrs (
+          map named stable
+          ++ map named nightly
+        )
+        // {
+          default = lib.last stable;
+          nightly = lib.last nightly;
+        };
+
+      packages = pkgs: let
+        filtered = lib.filterAttrs (name: rel: rel ? ${pkgs.hostPlatform.system}) releases;
+        package = name: rel: pkgs.callPackage ./package {zigRelease = rel;};
+      in
+        builtins.mapAttrs package filtered;
+    in
+      builtins.mapAttrs (system: packages) nixpkgs.legacyPackages;
   };
 }
