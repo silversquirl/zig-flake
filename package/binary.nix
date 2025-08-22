cbf478f3b4ce2a0e70659e3aee67d1a6f58babf6
Silver <silver@squirl.dev>
2025-08-22 10:45:06 +0100

    add setup hook and dependency fetcher

--- package/binary.nix
+++ package/binary.nix
@@ -2,21 +2,61 @@
 {
   lib,
   stdenvNoCC,
+  callPackage,
+  hostPlatform,
+  coreutils,
+  xcbuild,
   zigSource,
   zigVersion,
-}:
-stdenvNoCC.mkDerivation {
-  pname = "zig";
-  version = zigVersion;
-  meta = import ./meta.nix {
-    inherit lib;
+}: let
+  zig = stdenvNoCC.mkDerivation (finalAttrs: {
+    pname = "zig";
     version = zigVersion;
-    date = zigSource._date or null;
-  };
+    meta = import ./meta.nix {
+      inherit lib;
+      version = finalAttrs.version;
+      date = zigSource._date or null;
+    };
+    passthru = {
+      fetchDeps = callPackage ./fetch-deps.nix {inherit zig;};
+    };
 
-  src = zigSource;
-  installPhase = ''
-    install -Dt "$out/bin" zig
-    cp -r lib doc "$out"
-  '';
-}
+    src = zigSource;
+
+    # xcbuild provides xcode-select, which is required for SDK detection on macos
+    buildInputs = lib.optional hostPlatform.isDarwin xcbuild;
+    propagatedBuildInputs = lib.optional hostPlatform.isDarwin xcbuild;
+
+    # zig-flake's setup hook only supports Zig 0.12 or later due to using `--release`
+    # TODO: print an error in the setup hook, rather than silently disabling it
+    setupHook =
+      if (lib.versionAtLeast zig.version "0.12")
+      then ./setup-hook.sh
+      else null;
+
+    postPatch =
+      # Zig's build looks at /usr/bin/env to find dynamic linking info. This doesn't
+      # work in Nix's sandbox. Use env from our coreutils instead.
+      ''
+        substituteInPlace lib/std/zig/system.zig \
+          --replace-fail "/usr/bin/env" "${lib.getExe' coreutils "env"}"
+      ''
+      # Zig tries to access xcrun and xcode-select at the absolute system path to query the macOS SDK
+      # location, which does not work in the darwin sandbox.
+      # Upstream issue: https://github.com/ziglang/zig/issues/22600
+      # Note that while this fix is already merged upstream and will be included in 0.14+,
+      # we can't fetchpatch the upstream commit as it won't cleanly apply on older versions,
+      # so we substitute the paths instead.
+      + lib.optionalString (hostPlatform.isDarwin && lib.versionOlder finalAttrs.version "0.14") ''
+        substituteInPlace lib/std/zig/system/darwin.zig \
+          --replace-fail /usr/bin/xcrun xcrun \
+          --replace-fail /usr/bin/xcode-select xcode-select
+      '';
+
+    installPhase = ''
+      install -Dt "$out/bin" zig
+      cp -r lib doc "$out"
+    '';
+  });
+in
+  zig
