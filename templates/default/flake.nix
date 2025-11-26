9f5146e9eaba71c5ae60d9dfee0ff7d0acedbd87
Silver <silver@squirl.dev>
2025-11-26 18:29:57 +0000

    use zig 0.15.2 for default template

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -12,7 +12,7 @@
   }: let
     forAllSystems = f:
       builtins.mapAttrs
-      (system: pkgs: f pkgs zig.packages.${system}.zig_0_15_1)
+      (system: pkgs: f pkgs zig.packages.${system}.zig_0_15_2)
       nixpkgs.legacyPackages;
   in {
     devShells = forAllSystems (pkgs: zig: {
